71472d967d22594df22e5721530bb1e491083def
plancktimesoftware <pranavkumar1108@gmail.com>
2024-06-30 14:05:23 +0530

    Adding more Window methods

--- include/Window.h
+++ include/Window.h
@@ -10,6 +10,19 @@ namespace SWApi
 	struct Rect;
 	class Module;
 
+	/**
+	 * Enum containing special z-order values supported by Windows.
+	 * See https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowpos
+	 * Type is intptr_t so it can be reinterpret_cast to HWND.
+	 */
+	enum class ZOrder : intptr_t
+	{
+		kTop = 0, //HWND_TOP
+		kBottom = 1, //HWND_BOTTOM
+		kTopMost = -1, //HWND_TOPMOST
+		kNoTopMost = -2	//HWND_NOTOPMOST
+	};
+
 	class Window
 	{
 	public:
@@ -21,8 +34,28 @@ namespace SWApi
 			std::optional<Module> associatedModule,
 			std::optional<LPVOID> lpParam) -> WinResult<Window>;
 
-		auto GetRect() const -> WinResult<Rect>;
+		auto IsEnabled() const -> bool;
+		auto IsVisible() const -> bool;
+		auto Enable(bool bEnable) -> bool;
+		/**
+		 * Sets the window's show state.
+		 * \param nCmdShow Controls how the window is to be shown.
+		 *		  See https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+		 * \return If the window was previously visible, returns true.
+		 *		   If the window was previously hidden, returns false.
+		 */
 		auto Show(int nCmdShow) -> bool;
+
+		auto GetRect() const->WinResult<Rect>;
+		auto SetRect(Rect rect) const -> WinResult<void>;
+		auto SetZOrderRelative(Window windowInsertAfter) -> WinResult<void>;
+		auto SetZOrderAbsolute(ZOrder zorder) -> WinResult<void>;
+
+		auto GetStyleFlags() -> WinResult<DWORD>;
+		auto SetStyleFlags(DWORD styleFlags) -> WinResult<void>;
+		auto GetExStyleFlags() -> WinResult<DWORD>;
+		auto SetExStyleFlags(DWORD exStyleFlags) -> WinResult<void>;
+
 		void Update();
 
 		Window() = default;
@@ -31,7 +64,6 @@ namespace SWApi
 		~Window() {}
 
 		auto GetNative() const -> HWND { return mHWNDSharedPtr.get(); }
-
 		void SetNative(HWND hwnd);
 	private:
 		using HWNDSharedPtr = std::shared_ptr<HWND__>;
--- source/Window.cpp
+++ source/Window.cpp
@@ -29,6 +29,26 @@ namespace SWApi
 		return win;
 	}
 
+	auto Window::IsEnabled() const -> bool
+	{
+		return GetNative() && IsWindowEnabled(GetNative());
+	}
+
+	auto Window::IsVisible() const -> bool
+	{
+		return GetNative() && IsWindowVisible(GetNative());
+	}
+
+	auto Window::Enable(bool bEnable) -> bool
+	{
+		return GetNative() && EnableWindow(GetNative(), bEnable ? TRUE : FALSE);
+	}
+
+	auto Window::Show(int nCmdShow) -> bool
+	{
+		return GetNative() && ShowWindow(GetNative(), nCmdShow);
+	}
+
 	auto Window::GetRect() const -> WinResult<Rect>
 	{
 		RECT rect{};
@@ -38,9 +58,73 @@ namespace SWApi
 		return Rect(rect);
 	}
 
-	auto Window::Show(int nCmdShow) -> bool
+	auto Window::SetRect(Rect rect) const -> WinResult<void>
 	{
-		return ShowWindow(GetNative(), nCmdShow);
+		BOOL result = SetWindowPos(GetNative(), nullptr,
+								   rect.left, rect.right, rect.Width(), rect.Height(),
+								   SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOZORDER);
+		if (result == FALSE)
+			return Err(HRESULT_FROM_WIN32(GetLastError()));
+		return WinResult<void>();
+	}
+
+	auto Window::SetZOrderRelative(Window windowInsertAfter) -> WinResult<void>
+	{
+		BOOL result = SetWindowPos(GetNative(), windowInsertAfter.GetNative(),
+								   0, 0, 0, 0,
+								   SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE);
+		if (result == FALSE)
+			return Err(HRESULT_FROM_WIN32(GetLastError()));
+		return WinResult<void>();
+	}
+
+	auto Window::SetZOrderAbsolute(ZOrder zorder) -> WinResult<void>
+	{
+		HWND zorderHwnd = reinterpret_cast<HWND>(zorder);
+		BOOL result = SetWindowPos(GetNative(), zorderHwnd,
+								   0, 0, 0, 0,
+								   SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE);
+		if (result == FALSE)
+			return Err(HRESULT_FROM_WIN32(GetLastError()));
+		return WinResult<void>();
+	}
+
+	auto Window::GetStyleFlags() -> WinResult<DWORD>
+	{
+		SetLastError(NOERROR);
+		LONG_PTR value = GetWindowLongPtr(GetNative(), GWL_STYLE);
+		if (value == 0 && GetLastError() != NOERROR)
+			return Err(HRESULT_FROM_WIN32(GetLastError()));
+		return static_cast<DWORD>(value);
+	}
+
+	auto Window::SetStyleFlags(DWORD styleFlags) -> WinResult<void>
+	{
+		SetLastError(NOERROR);
+		LONG_PTR result = SetWindowLongPtr(GetNative(), GWL_STYLE,
+										   static_cast<LONG_PTR>(styleFlags));
+		if (result == 0 && GetLastError() != NOERROR)
+			return Err(HRESULT_FROM_WIN32(GetLastError()));
+		return WinResult<void>();
+	}
+
+	auto Window::GetExStyleFlags() -> WinResult<DWORD>
+	{
+		SetLastError(NOERROR);
+		LONG_PTR value = GetWindowLongPtr(GetNative(), GWL_EXSTYLE);
+		if (value == 0 && GetLastError() != NOERROR)
+			return Err(HRESULT_FROM_WIN32(GetLastError()));
+		return static_cast<DWORD>(value);
+	}
+
+	auto Window::SetExStyleFlags(DWORD exStyleFlags) -> WinResult<void>
+	{
+		SetLastError(NOERROR);
+		LONG_PTR result = SetWindowLongPtr(GetNative(), GWL_EXSTYLE,
+										   static_cast<LONG_PTR>(exStyleFlags));
+		if (result == 0 && GetLastError() != NOERROR)
+			return Err(HRESULT_FROM_WIN32(GetLastError()));
+		return WinResult<void>();
 	}
 
 	void Window::Update()
